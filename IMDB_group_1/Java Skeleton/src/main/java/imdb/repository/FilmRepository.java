a89407a59dc98a916913b493a2248e888cf3e36f
Invincible9 <Invulnerable89@abv.bg>
2018-04-21 12:04:13 +0300

    delete imdb

--- IMDB_group_1/Java Skeleton/src/main/java/imdb/repository/FilmRepository.java
+++
@@ -1,9 +0,0 @@
-package imdb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import imdb.entity.Film;
-
-import java.util.List;
-
-public interface FilmRepository extends JpaRepository<Film, Integer> {
-}
